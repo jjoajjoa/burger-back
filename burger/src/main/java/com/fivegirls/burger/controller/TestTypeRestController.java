933304300058b9ee041f8ecfdea2e10dcacb762e
ljh211118 <134677494+ljh211118@users.noreply.github.com>
2024-09-20 11:02:09 +0900

    Update TestTypeRestController.java

--- burger/src/main/java/com/fivegirls/burger/controller/TestTypeRestController.java
+++ burger/src/main/java/com/fivegirls/burger/controller/TestTypeRestController.java
@@ -10,8 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-
-public class TestTypeRestController {
+public class TestTypeController {
 
     @Autowired
     TestTypeDAO dao;
@@ -21,19 +20,22 @@ public class TestTypeRestController {
 
     // 결과 저장
     @PostMapping("/insertTest")
-    public TestTypeVO insertTestType(@RequestBody TestTypeVO vo) {
-       int sessionUserPk = (int) session.getAttribute("userPk");
-       vo.setUserPk(sessionUserPk);
+    public void insertTestType(@RequestBody TestTypeVO vo) {
+    	int sessionUserPk = (int) session.getAttribute("userPk");
+    	System.out.println("sessionUserPk >>>>" + sessionUserPk);
         dao.insertTestType(vo);
-        return vo;
+        System.out.println("/api/insertTest" + vo);
+        vo.toString();
     }
 
     // 사용자 PK와 답변으로 결과 조회 (GET 방식)
     @GetMapping("/getTest")
     public TestTypeVO getTestTypeByUser() {
-       int sessionUserPk = (int) session.getAttribute("userPk");
-       TestTypeVO test = dao.getTestTypeByUser(userPk);
-       test.setSessionUserPk(sessionUserPk);
+    	int sessionUserPk = (int) session.getAttribute("userPk");
+    	System.out.println("sessionUserPk >>>>" + sessionUserPk);
+    	TestTypeVO test = dao.getTestTypeByUser(sessionUserPk);
+    	test.setSessionUserPk(sessionUserPk);
+    	System.out.println("/api/getTest" + test);
         return test;
     }
     
